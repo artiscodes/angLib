78c1cf98eefc3bd327f6846719592a32a8123758
Art <artiscodes@gmail.com>
2020-01-29 19:36:23 +0100

    first lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LibraryComponent } from './library/library.component';
+import { MyLibModule } from 'my-lib';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibraryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MyLibModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
